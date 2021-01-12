58c223a6af9c2cf88f3077f54cc65b9b2e523811
Suwat Inkaew <swik999@gmail.com>
2021-01-12 14:43:25 +0700

    Change

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,29 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+	
+	cout << "Press Enter 3 times to reveal your future.";
+	cin.get();
+	cin.get();
+	cin.get();
+	srand(time(0));
+	int x = rand()%9;
+	cout << "You will get " ;
+	if(x == 0) cout <<  "A";
+	else if(x == 1) cout << "B+";
+	else if(x == 2) cout << "B";
+	else if(x == 3) cout << "C+";
+	else if(x == 4) cout << "C";
+	else if(x == 5) cout << "D+";
+	else if(x == 6) cout << "D";
+	else if(x == 7) cout << "F";
+	else cout << "W";
+	cout << " in this 261102.";
+	
+	return 0;
+}
+
